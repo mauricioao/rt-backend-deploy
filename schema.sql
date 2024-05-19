f55913364116fcf7b4a953ff4aa3831db4a1c96e
Mauricio Aguirre <mauricio.aguirreo@outlook.com>
2024-05-19 10:32:06 -0500

    add cors

--- schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
-	email varchar(255) NOT NULL UNIQUE,
-	password varchar(60) NOT NULL,
-    role varchar(255) NOT NULL DEFAULT 'user'
-);
-
-INSERT INTO users (email, password, role)
-VALUES ('codeable@example.com', '$2b$10$mGvE0s4A4fGAAesZ8ZRPoOrDyi.Ui2/tjF8nTmq4/tL7kfC8blcla', 'admin');
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
+import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRouter from "./routers/auth-router";
 import errorHandler from "./middlewares/error";
-
+import cors from "cors";
 
 const app = express();
 const port = process.env["PORT"] || 5500;
 
+const corsOptions = {
+  origin: process.env["CLIENT_ORIGIN"],
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
  
